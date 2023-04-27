12d2587948f789eca655c74bee33625c32e246b0
HappyYusuke <c1005073@planet.kanazawa-it.ac.jp>
2023-04-27 21:07:43 +0900

    制作途中

--- happymini_manipulation/happymini_manipulation/motor_controller.py
+++ happymini_manipulation/happymini_manipulation/motor_controller.py
@@ -13,6 +13,8 @@ class JointController(Node):
         super().__init__('joint_controller_node')
         # Publisher
         self.joint_pub = self.create_publisher(JointTrajectory, 'joint_trajectory_controller/joint_trajectory', 10)
+        # Subscriber
+        self.create_subscription()
         # Value
         self.joint_angle_list = []
         self.gripper_close = False
@@ -24,9 +26,6 @@ class JointController(Node):
                 'gripper'
                 ]
     
-    def deg_to_rad(self, deg):
-        return math.radians(deg)
-
     def inverse_kinematics(self, coordinate):
         x = coordinate[0]
         y = coordinate[1]
@@ -41,16 +40,21 @@ class JointController(Node):
             shoulder_angle = -1*math.acos((x*x+y*y+l1*l1-l2*l2) / (2*l1*math.sqrt(x*x+y*y))) + math.atan(y/x)
             elbow_angle = math.atan((y-l1*math.sin(shoulder_angle))/(x-l1*math.cos(shoulder_angle)))-shoulder_angle - math.pi/2
             wrist_angle = -1*(shoulder_angle + elbow_angle) - math.pi/2
-            angle_list = list(map(math.degrees, [0.0, shoulder_angle, elbow_angle, wrist_angle, self.deg_to_rad(-90)]))
-            print(angle_list)
+            # gripper
+            if self.gripper_close:
+                gripper = math.radians(55)
+            else:
+                gripper = math.radians(-90)
+            angle_list = list(map(math.degrees, [0.0, shoulder_angle, elbow_angle, wrist_angle, gripper]))
             return angle_list
         except ValueError:
             self.get_logger().info('Can not move arm.')
             return [numpy.nan]*5
 
     def publish_joint(self, joint_angle, execute_time=2):
-        self.get_logger().info('Publish joint.')
-        joint_angle = list(map(self.deg_to_rad, joint_angle))
+        # deg => rad
+        joint_angle = list(map(math.radians, joint_angle))
+        # メッセージの作成
         msg = JointTrajectory()
         msg.header.stamp = self.get_clock().now().to_msg()
         msg.joint_names = self.joint_names
@@ -58,23 +62,32 @@ class JointController(Node):
         msg.points[0].positions = list(map(float, joint_angle))
         msg.points[0].time_from_start = Duration(
                 seconds=int(execute_time), nanoseconds=(execute_time-int(execute_time))*1e9).to_msg()
+        # パブリッシュ
+        self.get_logger().info('Publish joint.')
         self.joint_pub.publish(msg)
         time.sleep(execute_time + 0.5)
 
-    def manipulation(self, coordinate):
-        self.joint_angle_list = self.inverse_kinematics(coordinate)
-        self.publish_joint(self.joint_angle_list, 2)
-
     def gripper(self, gripper_close):
-        if gripper_close:
+        self.gripper_close = gripper_close
+        if self.gripper_close:
             self.joint_angle_list[4] = 55
         else:
             self.joint_angle_list[4] = -90
         self.publish_joint(self.joint_angle_list, 2)
 
-    def start_up(self):
-        self.joint_angle_list = [0.0, -90, 90, 0.0, -90]
-        self.publish_joint(self.joint_angle_list)
+
+#class ManipulateArm(JointController):
+#    def __init__(self):
+#        super(ManipulateArm, self).__init__()
+#        
+#
+#    def manipulation(self, coordinate):
+#        self.joint_angle_list = self.inverse_kinematics(coordinate)
+#        self.publish_joint(self.joint_angle_list, 2)
+#
+#    def start_up(self):
+#        self.joint_angle_list = [0.0, -90, 90, 0.0, -90]
+#        self.publish_joint(self.joint_angle_list)
 
     
 
@@ -83,8 +96,8 @@ def main():
     rclpy.init()
     jc_node = JointController()
     #jc_node.send_goal()
-    #jc_node.start_up()
-    jc_node.manipulation([0.3, 0.5])
+    jc_node.start_up()
+    #jc_node.manipulation([0.3, 0.5])
     #jc_node.manipulation([0.3, 0.4])
     #jc_node.gripper(False)
     #jc_node.manipulation([0.3, 0.45])
