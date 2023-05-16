071fd63df33955ebeac974a8fd910a147f617ea2
Yusuke_Kanazawa <c1005073@planet.kanazawa-it.ac.jp>
2023-05-16 20:11:37 +0900

    hand_poseをdetect_poseに入れるためのパッケージ

---
+++ detect_pose/detect_pose/detect_pose_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from detect_pose.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ detect_pose/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'detect_pose'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='demulab',
+    maintainer_email='c1005073@planet.kanazawa-it.ac.jp',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'detect_pose_node = detect_pose.detect_pose_node:main'
+        ],
+    },
+)
