83fab3d7c3ee44a56efc11addf83c94b23db6675
HappyYusuke <c1005073@planet.kanazawa-it.ac.jp>
2023-05-26 17:41:16 +0900

    voice_testの改良

--- rcj_2023_master/rcj_2023_master/voice_test.py
+++ rcj_2023_master/rcj_2023_master/voice_test.py
@@ -1,16 +1,18 @@
 import rclpy
 from rclpy.node import Node
-from happymini_msgs.srv import SpeechToText
+from happymini_msgs.srv import SpeechToText, NameDetect
 
 class TestClient(Node):
     def __init__(self):
         super().__init__('test_client')
         self.stt_srv = self.create_client(SpeechToText, 'stt')
-        while not self.stt_srv.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info("bag_localization_server is not here ...")
+        self.nd_srv = self.create_client(NameDetect, 'nd')
+        while not self.stt_srv.wait_for_service(timeout_sec=1.0) and self.nd_srv.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("Message is not here ...")
         self.stt_srv_req = SpeechToText.Request()
+        self.nd_srv_req = NameDetect.Request()
 
-    def send_request(self, tex='start'):
+    def stt_send_request(self, tex='start'):
         stt_srv_result = 'None'
         self.stt_srv_req.tex = 'start'
 
@@ -18,19 +20,37 @@ class TestClient(Node):
         while not stt_srv_future.done() and rclpy.ok():
             rclpy.spin_once(self, timeout_sec=0.1)
         if stt_srv_future.result() is not None:
-            stt_srv_result = stt_srv_future.result()
+            stt_srv_result = stt_srv_future.result().result
             print(stt_srv_result)
             return stt_srv_result
         else:
             self.get_logger().info(f"Service call failed")
             return None
 
+    def nd_send_request(self, text=None):
+        nd_srv_result = 'None'
+        self.nd_srv_req.text = text
+
+        nd_srv_future = self.nd_srv.call_async(self.nd_srv_req)
+        while not nd_srv_future.done() and rclpy.ok():
+            rclpy.spin_once(self, timeout_sec=0.1)
+        if nd_srv_future.result() is not None:
+            nd_srv_result = nd_srv_future.result().result
+            print(nd_srv_result)
+            return nd_srv_result
+        else:
+            self.get_logger().info(f"Service call failed")
+            return None
+
+
+
+
 def main():
     rclpy.init()
     tc = TestClient()
     try:
-        tc.send_request()
-        print(SpeechToText.Response())
+        stt = tc.stt_send_request()
+        tc.nd_send_request(stt)
         rclpy.spin_once(tc)
     except KeyboardInterrupt:
         pass
