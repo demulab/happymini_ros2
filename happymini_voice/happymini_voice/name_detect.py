261aa0f9d3e39f494e1a815bb8c9be1988bc437f
Masa-ki-Onishi <c1000300@planet.kanazawa-it.ac.jp>
2023-06-05 19:51:37 +0900

    name_detect 改良

--- happymini_voice/happymini_voice/name_detect.py
+++ happymini_voice/happymini_voice/name_detect.py
@@ -12,11 +12,12 @@ class NameDetectServer(Node):
     def __init__(self):
         super().__init__('name_detect')
         self.create_service(NameDetect, 'nd', self.calculate_similarity)
+        self.word_list = ["amelia", "angel", "ava", "charlie", "charlotte", "hunter", "max", "mia", "olivia", "parker", "sam", "jack", "noah", "thomas", "whilliam"]
+
 
     def calculate_similarity(self, srv_req, srv_res):
         target_word = srv_req.text.lower()
-        word_list = ["amelia", "angel", "ava", "charlie", "charlotte", "hunter", "max", "mia", "olivia", "parker", "sam", "jack", "noah", "thomas", "whilliam"]
-
+        
         print("Target Word:", target_word)
         
         target_vector = np.array([1 if ch in target_word else 0 for ch in sorted(set(target_word))])
@@ -24,21 +25,21 @@ class NameDetectServer(Node):
         best_sim = -1
         srv_res.result ='None'
         
-        for word in word_list:
+        for word in self.word_list:
             word_vector = np.array([1 if ch in word else 0 for ch in sorted(set(target_word))]) # 修正点
             
             levenshtein_dist = edit_distance(target_word, word)
-            jaccard_dist = jaccard_distance(set(ngrams(target_word, 3)), set(ngrams(word, 3)))
+            #jaccard_dist = jaccard_distance(set(ngrams(target_word, 3)), set(ngrams(word, 3)))
             cosine_dist = cosine_distance(target_vector, word_vector)
             
             levenshtein_sim = 100 - (levenshtein_dist / max(len(target_word), len(word))) * 100
-            jaccard_sim = (1 - jaccard_dist) * 100
+            #jaccard_sim = (1 - jaccard_dist) * 100
             cosine_sim = (1 - cosine_dist) * 100
-            all_sim = levenshtein_sim * 0.2+ jaccard_sim * 0.1 + cosine_sim * 0.7
+            all_sim = levenshtein_sim * 0.2 + cosine_sim * 0.8
             
             print("Word:", word)
             print("Levenshtein Similarity:", levenshtein_sim)
-            print("Jaccard Similarity:", jaccard_sim)
+            #print("Jaccard Similarity:", jaccard_sim)
             print("Cosine Similarity:", cosine_sim)
             print("Similarity:", all_sim)
             print()
@@ -48,6 +49,9 @@ class NameDetectServer(Node):
                 best_word = word
         if best_sim > 70:
             srv_res.result = best_word
+        if srv_res.result != 'None':
+            self.word_list.remove(best_word)
+            print(self.word_list)
         print("Best Word:", srv_res.result)
         print("Similarity:", best_sim)
         return srv_res
