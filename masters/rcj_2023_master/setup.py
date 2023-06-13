80cbfda9964d4321a7cd972e40ab19ce1f08f81a
Kensei Demura <k.demura@qibitech.com>
2023-06-13 12:41:50 +0900

    add launch

--- masters/rcj_2023_master/setup.py
+++ masters/rcj_2023_master/setup.py
@@ -9,6 +9,8 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
+        (os.path.join('share', package_name, 'launch'),
+         glob('launch/*.launch.xml')),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
